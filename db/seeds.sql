15a255304e6e6e1412292c3b9e17e681a5166ffc
Garrett Griffin <griffin.garrett@gmail.com>
2019-06-01 17:13:22 -0500

    created initial files and mySQL database

---
+++ db/seeds.sql
@@ -0,0 +1,10 @@
+USE burgers_db;
+
+INSERT INTO burgers (burger_name)
+VALUES ('Whopper');
+
+INSERT INTO burgers (burger_name)
+VALUES ('Double bacon cheese');
+
+INSERT INTO burgers (burger_name)
+VALUES ('Big Mac');
